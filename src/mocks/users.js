0cdc6c1ae133bb771c04845323945777c401debb
your_username <guilhermev2huehue@gmail.com>
2023-02-16 20:44:07 -0300

    first commit

---
+++ src/mocks/users.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 1,
+    name:'gui'
+  },
+  {
+    id: 2,
+    name:'lirio'
+  },
+  {
+    id: 3,
+    name:'mosh'
+  },
+  {
+    id: 4,
+    name:'gabs'
+  },
+  {
+    id: 5,
+    name:'lais'
+  },
+  {
+    id: 6,
+    name:'lu'
+  },
+];
